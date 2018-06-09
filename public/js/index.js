604f45f4a4f9000768c191ae87b0008a678a5202
Andras Nett <andras.nett@gmail.com>
2018-06-09 15:50:47 +0200

    Add from for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -4,13 +4,25 @@ socket.on('connect', function () {
     console.log('Connected to server');
 });
 
-socket.on('newMessage', function (message) {
-    console.log('New message', message);
-});
-
 socket.on('disconnect', function () {
     console.log('Disconnected from server');
 });
 
+socket.on('newMessage', function (message) {
+    console.log('New message', message);
+    const li = $('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    $('#messages').append(li);
+});
 
+$('#message-form').on('submit', function (e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: $('[name=message]').val()
+    }, function (data) {
+        console.log(data);
+    });
+});
 
