0126f9edd9ca9539bafd270036eabc29a5474532
Andras Nett <andras.nett@gmail.com>
2018-06-09 13:25:40 +0200

    Greet new user, and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -4,11 +4,13 @@ socket.on('connect', function () {
     console.log('Connected to server');
 });
 
+socket.on('newMessage', function (message) {
+    console.log('New message', message);
+});
+
 socket.on('disconnect', function () {
     console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function (message) {
-    console.log('New message', message);
-});
+
 
